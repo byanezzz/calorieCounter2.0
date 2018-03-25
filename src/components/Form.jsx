99cb00e30a23958541f10a3241fd8d0db372221f
Jose <Jgta1987@gmail.com>
2018-03-24 23:32:40 -0300

    update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
 
-export default class Form extends Component {
-  render() {
-    return (
-      <div className="form">
-        <form action="">
-        <div classame="row">
-        <div>
-          <label htmlFor="">Nombre</label>
-        </div>
-        <div>
-            <input type="text" />
-          </div>
-        </div>
-        </form>
-        
-      </div>
-    )
-  }
-}
+export default class Form extends Component { 
+  render() { 
+    return ( 
+      
+      <form> 
+        <div> 
+          <label htmlFor="genero">Masculino</label> 
+          <input type="radio" name="genero" id="genero"/> 
+          <label htmlFor="genero">Femenino</label> 
+          <input type="radio" name="genero" id="genero"/> 
+        </div> 
+        <div> 
+          <label htmlFor="edad">Edad</label> 
+          <input type="text" name="edad" id="edad"/> 
+          <label htmlFor="peso">Peso, kg</label> 
+          <input type="text" name="peso" id="peso"/> 
+          <label htmlFor="altura">Altura, cm</label> 
+          <input type="text" name="altura" id="altura"/> 
+        </div> 
+        <div> 
+          <h5>Tipo de Actividad</h5> 
+          <div> 
+            <input type="radio" name="actividad" id="sedentario"/> 
+            <label htmlFor="sedentario">Sedentaria</label> 
+          </div> 
+          <div> 
+            <input type="radio" name="actividad" id="ligero"/> 
+            <label htmlFor="ligero">Actividad Ligera (1-3 veces por semana)</label> 
+          </div> 
+          <div> 
+            <input type="radio" name="actividad" id="moderano"/> 
+            <label htmlFor="moderano">Actividad Moderada (3-5 veces por semana)</label> 
+          </div> 
+          <div> 
+            <input type="radio" name="actividad" id="intenso"/> 
+            <label htmlFor="intenso">Actividad Intensa (6-7 veces por semana)</label> 
+          </div> 
+          <div> 
+            <input type="radio" name="actividad" id="atleta"/> 
+            <label htmlFor="atleta">Atleta Profesional</label> 
+          </div> 
+        </div> 
+        <div> 
+          <input type="submit" value="Siguiente"/> 
+        </div> 
+      </form> 
+    ) 
+  } 
+} 
