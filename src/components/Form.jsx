cc320a4a573f19dc53339b50be4d649b476c3e09
Jose <Jgta1987@gmail.com>
2018-03-24 23:21:21 -0300

    Auto stash before merge of "master" and "byanezzz/master"

---
+++ src/components/Form.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Form extends Component {
+  render() {
+    return (
+      <div className="form">
+        <form action="">
+        <div classame="row">
+        <div>
+          <label htmlFor="">Nombre</label>
+        </div>
+        <div>
+            <input type="text" />
+          </div>
+        </div>
+        </form>
+        
+      </div>
+    )
+  }
+}
