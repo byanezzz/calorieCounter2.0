f34607823e2f15a013ef276307d1496916035298
byanezzz <biancayanez89@gmail.com>
2018-03-24 23:41:57 -0300

    componente boton

---
+++ src/components/btn.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class btn extends Component {
+  render() {
+    return (
+      <div className="btn">
+        <input type="button" value={txt_btn}/>
+      </div>
+    )
+  }
+}
